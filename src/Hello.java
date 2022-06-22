7ffae3de32f8e043ea87e670a60a336793575d3f
Solomon Ko <solomonko@MacBook-Air-Solomon.local>
2022-06-22 18:50:43 +0900

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+    public static void main (String[] args){
+        byte num = 100; // 8bit -128 + 127
+        short num1 = 131; // 16bit  -32768 + 32767
+        int num2 = 141; // 32bit    -2147483648 + 2147483647
+        long num3 = 123; // 64bit   -9 223 372 036 854 775 808
+
+        float num4 = 1.1f; // 32 6 symbols
+        double num5 = 1.11; // 64 15 symbols
+
+        char sym = '\u2122'; // 16 bit
+        boolean isTrue = false; // 1bit true
+    }
+
+}
